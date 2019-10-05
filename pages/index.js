8a7110b34feada936aefe4b6a3e01fa97463876f
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-05 18:00:49 -0500

    Add header to index page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Head from "next/head";
 import NewSwagForm from "../components/NewSwagForm";
+import Header from "../components/Header";
 
 const Home = () => (
-  <div>
+  <>
     <Head>
       <title>FreeSwag@NU</title>
       <link rel="icon" href="/static/favicon.ico" />
     </Head>
 
-    <div className="hero">
-      <NewSwagForm />
-    </div>
-  </div>
+    <main>
+      <Header />
+
+    </main>
+  </>
 );
 
 export default Home;
