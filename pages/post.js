446d1024bb662408c3c4ae91486f4329831b7484
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-06 21:22:23 -0500

    Connect new swag form to posting page

---
+++ pages/post.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NewSwagForm from "../components/NewSwagForm";
+import Layout from "../components/Layout";
+
+const PostSwag = () => {
+  return (
+    <Layout>
+      <NewSwagForm />
+    </Layout>
+  );
+};
+
+export default PostSwag;
